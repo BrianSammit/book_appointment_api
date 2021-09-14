07da9a964086db459791145f86cc8f478808c7d0
BrianSammit <briansammit.cruz@gmail.com>
2021-09-13 23:19:06 -0500

    Fix session store to work on production enviroment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "api_book_app.herokuapp.com"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "api_book_app.herokuapp.com"
+else 
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
