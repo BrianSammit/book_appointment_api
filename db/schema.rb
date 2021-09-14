cc59962e164e7ed4beea1a709987ec71e7b21908
BrianSammit <briansammit.cruz@gmail.com>
2021-09-14 18:56:24 -0500

    Change column name user to username

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_09_14_025516) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "user"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
