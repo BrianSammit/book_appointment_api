71cddd88334201446ae4fb184d29264d38ecea1f
BrianSammit <briansammit.cruz@gmail.com>
2021-09-13 21:36:00 -0500

    Configure cors

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,8 @@
+class StaticController < ApplicationController
+    
+    def home
+        render json: {status: "It's working"}
+        
+    end
+    
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "api_book_app.herokuapp.com"
