96fb4178c195eaba8bded1a2a8e42d2b4a907725
BrianSammit <briansammit.cruz@gmail.com>
2021-09-23 15:39:32 -0500

    Add the show method in appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,11 @@
 class AppointmentsController < ApplicationController
     def index
         appointment = Appointment.all
-        render json: appointment.to_json(except: [:created_at, :updated_at], include: {user: {only: [:username]}})
+        render json: appointment.to_json(except: [:created_at, :updated_at], include: [{user: {only: [:username]}},{skateboard: {only: [:brand]}}])
+    end
+
+    def show
+        appointment = Appointment.find(params[:id])
+        render json:  appointment.to_json(except: [:created_at, :updated_at], include: [{user: {only: [:username]}},{skateboard: {only: [:brand]}}])
     end
 end
