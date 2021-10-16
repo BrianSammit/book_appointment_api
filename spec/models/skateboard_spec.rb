43b38fd4c05c5d93ef9e9907bd7dfad074b1b360
BrianSammit <briansammit.cruz@gmail.com>
2021-10-15 22:52:17 -0500

    Add skateboard rspec model

---
+++ spec/models/skateboard_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Skateboard, type: :model do
+  
+  describe 'associations' do
+    it { should have_many(:appointments)}
+   
+    it { should validate_presence_of(:image) }
+    it { should validate_presence_of(:brand) }
+
+  end
+
+  it "is valid with valid attributes" do
+    skate =  Skateboard.create(brand: 'Element', image: "https://res.cloudinary.com/ddjesec95/image/upload/v1632434678/Skate%20brands/element-skateboards-e1518473008825_xij1e7.jpg")
+    expect(skate).to be_valid
+  end
+
+  it "is valid with valid attributes" do
+    skate =  Skateboard.create(brand: nil, image: "https://res.cloudinary.com/ddjesec95/image/upload/v1632434678/Skate%20brands/element-skateboards-e1518473008825_xij1e7.jpg")
+    expect(skate).to_not be_valid
+  end
+
+  it "is valid with valid attributes" do
+    skate =  Skateboard.create(brand: 'Element', image: nil)
+    expect(skate).to_not be_valid
+  end
+
+end
+
