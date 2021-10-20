61392c603e874a09b91a116f6ae07570f84d5838
BrianSammit <briansammit.cruz@gmail.com>
2021-10-20 13:25:03 -0500

    Fix cokkies session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@
 #     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 # end
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: all, tld_length: 2
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: :all, tld_length: 2
 else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
