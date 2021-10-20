1c5c8d70c00ba18e60f3e2b97cf55e93ee23cfbe
BrianSammit <briansammit.cruz@gmail.com>
2021-10-20 12:32:54 -0500

    Fix cokkies session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,10 @@
-if Rails.env.production?
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://frozen-chamber-04304.herokuapp.com/"
-else 
+# if Rails.env.production?
+#     Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://frozen-chamber-04304.herokuapp.com/"
+# else 
+#     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+# end
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "frozen-chamber-04304.herokuapp.com"
+  else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
-end
+  end
