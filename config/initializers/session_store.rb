15b3226228e0e0f527683c860f7ecf9bf50423f5
BrianSammit <briansammit.cruz@gmail.com>
2021-10-19 23:30:38 -0500

    Fix storage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env == "production"
+if Rails.env.production?
     Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://frozen-chamber-04304.herokuapp.com/"
 else 
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
