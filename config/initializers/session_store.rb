1480c7ce118d7ff44f9b8f1371b221d844f75d5d
BrianSammit <briansammit.cruz@gmail.com>
2021-10-19 23:09:39 -0500

    Fix domain session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "api_book_app.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://frozen-chamber-04304.herokuapp.com"
 else 
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
