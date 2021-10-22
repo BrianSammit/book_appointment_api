d24af7400298af24be5cbfef7b6537b911e839bc
BrianSammit <briansammit.cruz@gmail.com>
2021-10-21 22:01:08 -0500

    fix cokkies probelm in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,8 +3,5 @@
 # else 
 #     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 # end
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: :all, tld_length: 2
-else
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
-end
+
+Rails.application.config.session_store :cookie_store, key: "_authentication_app"
